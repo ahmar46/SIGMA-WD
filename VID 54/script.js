d85f0a89b62992c9e6721561e07b66bbea5d9779
ahmar46 <ahmarfreelancer@gmail.com>
2025-08-04 18:44:38 +0530

    VID 54 Done

---
+++ VID 54/script.js
@@ -0,0 +1,24 @@
+alert("Hello World");
+
+console.log("Code is running...");
+console.log("Code is running...");
+console.log("Code is running...");
+
+var a = prompt("Enter a number:");
+
+var istrue = confirm("Are you sure you want to leave this page?");
+
+if(istrue) {
+    console.log("User confirmed to leave the page.");
+}
+
+else
+{
+    console.log("User chose to stay on the page.");
+}
+
+console.log("You entered: " + a);
+
+document.title = "Ka Chaow!";
+
+document.body.style.backgroundColor = "lightblue";
