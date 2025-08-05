8d17fc5e4891143667e10b9263009c9431d3ec77
ahmar46 <ahmarfreelancer@gmail.com>
2025-08-05 19:32:53 +0530

    VID 57 Done

---
+++ VID 57/index.js
@@ -0,0 +1,41 @@
+console.log("Hello World VID 57");
+
+
+// Basic For Loop Example
+// let a = 1;
+
+// for (let i = 0; i < 99; i++) {
+//   console.log(a + i);
+// }
+
+
+// For in loop Example
+let obj = {
+  name: "John",
+    age: 30,   
+    city: "New York"
+};
+
+// for in loop example
+// for (const key in obj) {
+//   console.log(key)
+// }
+
+// for of loop Example
+// for (const c of "john") {
+//   console.log(c);
+// }
+
+// while loop Example
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// do while loop Example
+let i = 25;
+do {
+  console.log(i);
+  i++;
+} while (i < 20);
