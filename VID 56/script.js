674ecec00e65ab4fdc7a7704d6aef8f274826b62
ahmar46 <ahmarfreelancer@gmail.com>
2025-08-05 18:49:48 +0530

    VID 56 Done

---
+++ VID 56/script.js
@@ -0,0 +1,21 @@
+console.log("Hello world");
+
+let age = 25;
+console.log("Age is: " + age);
+
+if (age >= 18) {
+    console.log("You are an adult.");
+}   else {
+    console.log("You are a minor.");
+}
+
+a= 6;
+b = 8;
+let c = a>b? (a-b) : (b-a);
+
+// // translate to 
+// if (a > b) {
+//    let  c = a - b;
+// }else {
+//     let c = b - a;
+// }
