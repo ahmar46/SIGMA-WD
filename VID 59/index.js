ce4d2cd5f8e11651d502d5d5fe2d3c238b156f81
ahmar46 <ahmarfreelancer@gmail.com>
2025-08-06 18:47:37 +0530

    VID 59 Excersize Question Added

---
+++ VID 59/index.js
@@ -0,0 +1,15 @@
+/*
+
+Create a faulty calculator using JavaScript.
+
+This faulty calculator will have the following features:
+1. It will take two numbers as input.
+2. it performs wrong calculations:
+    + --> -
+    * --> +
+    - --> /
+    / --> **
+
+    It performs wrong operations 10% of the time. 
+
+*/
