a5d298b98140121e69aee54eade2798d98f9c234
ahmar46 <ahmarfreelancer@gmail.com>
2025-08-06 19:26:18 +0530

    VID 61 running

---
+++ VID 61/index.js
@@ -0,0 +1,21 @@
+console.log("Faulty Calculator Loaded VID 61");
+
+/*
+
+Create a faulty calculator using JavaScript.
+
+This faulty calculator will have the following features:
+1. It will take two numbers as input.
+2. it performs wrong calculations:
+    + --> -
+    * --> +
+    - --> /
+    / --> **
+
+    It performs wrong operations 10% of the time. 
+
+*/
+
+let = Math.random();
+
+if 
