d9d144266c7c8e4fb9ae00d2a35ca692a46ce0b0
ahmar46 <ahmarfreelancer@gmail.com>
2025-08-07 14:28:06 +0530

    VID 62 Question Done

---
+++ index.js
@@ -0,0 +1,18 @@
+/* Create a Business Name Generator by combining list of adjectives and shop names and another.
+
+Adjectives:
+Crazy
+Amazing
+Fire
+
+Shop Names:
+Engine
+Foods
+Garments
+
+Another Word:
+Bros
+Limited
+Hub
+
+*/
