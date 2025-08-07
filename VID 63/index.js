ebb15b75e1a461d8d8095399a3a745964c98a56f
ahmar46 <ahmarfreelancer@gmail.com>
2025-08-07 19:11:27 +0530

    Array Vids running

--- VID 63/index.js
+++ VID 63/index.js
@@ -1,1 +1,9 @@
 console.log("JS ARRAYs VID 63.");
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(arr[0]); // Accessing the first element
+console.log(arr[1]); // Accessing the second element
+console.log(arr[2]); // Accessing the third element
+console.log(arr[3]); // Accessing the fourth element
+console.log(arr[4]); // Accessing the fifth element
