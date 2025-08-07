5568cbbccf9e17c6764fb0ac783df0533b74ad20
ahmar46 <ahmarfreelancer@gmail.com>
2025-08-07 19:37:43 +0530

    Arrays

--- VID 63/index.js
+++ VID 63/index.js
@@ -1,9 +1,17 @@
 console.log("JS ARRAYs VID 63.");
 
-let arr = [1, 2, 3, 4, 5];
-console.log(arr);
-console.log(arr[0]); // Accessing the first element
-console.log(arr[1]); // Accessing the second element
-console.log(arr[2]); // Accessing the third element
-console.log(arr[3]); // Accessing the fourth element
-console.log(arr[4]); // Accessing the fifth element
+let arr = [1, 2, 4, 5, 7];
+// console.log(arr);
+// console.log(arr[0]); // Accessing the first element
+// console.log(arr[1]); // Accessing the second element
+// console.log(arr[2]); // Accessing the third element
+// console.log(arr[3]); // Accessing the fourth element
+// console.log(arr[4]); // Accessing the fifth element
+// console.log(arr.length); // Getting the length of the array
+// console.log(arr, typeof arr); // Logging the array and its type
+
+arr[0] = 4646;
+
+console.log(arr.toString()); // Converting array to string
+console.log(arr.join(" and ")); // Joining array elements with a separator
+
