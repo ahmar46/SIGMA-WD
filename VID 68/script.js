a5ad227f8dd15dac999dec928f3e1cc6bf7cc731
ahmar46 <ahmarfreelancer@gmail.com>
2025-08-13 18:57:50 +0530

    VID 68 running

---
+++ VID 68/script.js
@@ -0,0 +1,8 @@
+console.log("Script loaded successfully!");
+
+// let boxes = document.getElementsByClassName("box");
+// console.log(boxes);
+
+// boxes[3].style.backgroundColor = "red";
+
+document.getElementById("red").style.backgroundColor = "red";
