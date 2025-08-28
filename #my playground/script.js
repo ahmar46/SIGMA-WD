9c5bf332cb704d0012d487de37a4b640819fff62
ahmar46 <ahmarfreelancer@gmail.com>
2025-08-28 18:55:14 +0530

    Code

--- #my playground/script.js
+++ #my playground/script.js
@@ -1,2 +1,8 @@
 console.log("For Javascript parctice");
 
+// reverse a string
+let name = "ahmar";
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverseString(name));
