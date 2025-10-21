2d6ed8abcd9540b9e25a0e8f447b7663dc0f9e49
ahmar46 <ahmarfreelancer@gmail.com>
2025-10-21 14:22:00 +0530

    VID 72 is done but not completly understood

--- VID 72/script.js
+++ VID 72/script.js
@@ -1,5 +1,16 @@
 console.log("Hello, World!");
 
-let boxes = document.getElementsByClassName("box")
+let boxes = document.getElementsByClassName("box");
+// let boxes = document.querySelector(".container").children;
 
-console.log(boxes)
+function getrandomcolor(){
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+Array.from(boxes).forEach(e=>{
+    e.style.backgroundColor = getrandomcolor();
+    e.style.color = getrandomcolor();
+})
